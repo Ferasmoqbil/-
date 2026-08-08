1fc2241dea9be0ab1506406ccf64192073a18d6c
asyl68372-wq <asyl68372@gmail.com>
2026-08-08 01:29:28 +0000

    غيرت اسم الاندكس الاول, واضفت كود متقدم عن الحلقه 44

---
+++ lesson 44 (ashil).py
@@ -0,0 +1,40 @@
+# فقرة الاسئله
+
+
+days = int(input('which day u have born? (1-31)')).strip()
+months = int(input('which month u have born? (1-12)')).strip()
+years = int(input('which year u have born? ')).strip()
+
+
+# يعطي الكود تاريخ اليوم عشان يقدر يحسب كم عمره بالضبط
+
+
+year_now = 2026
+month_now = 8+1
+day_now = 6+1
+years -= year_now
+months -= month_now
+days -= day_now
+print(f'You are {abs(years)} years, {abs(months)} months, and {abs(days)} days old u have to ')
+
+
+# كود إضافي توضعه في نهاية الكود الأساسي تماماً
+# (يحسب كم باقي له من أيام وشهور ليدخل السنة التي تلي عمره الحالي)
+
+
+current_age_years = abs(years)
+current_age_months = abs(months)
+current_age_days = abs(days)
+
+
+# حساب ما تبقى لإكمال السنة القادمة (مثلاً لو عمره 16 و 8 أشهر، نحسب كم باقي ليكمل 17 سنة)
+
+
+rem_months = 11 - current_age_months
+rem_days = 30 - current_age_days
+
+
+#print
+
+
+print(f"you have to end this year:{rem_months} mounth and {rem_days} days")
