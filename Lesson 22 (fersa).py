e383bc18eadf8f0446ec407414e018cfce3f5ed6
MangaLeader <152031090+Ferasmoqbil@users.noreply.github.com>
2026-08-08 14:05:56 +0000

    مشروع الدرس 22 (feras)

---
+++ Lesson 22 (fersa).py
@@ -0,0 +1,28 @@
+my_friends =["ashil", "mohammed", "haytham", "glal"]
+print(my_friends)
+my_friends.append("ali")
+print(my_friends)
+print(type(my_friends))
+
+
+x=[1, 2, 3, 4, 5, 6, 92, 86, 67, -514]
+print(x)
+x.extend(my_friends)
+print(x)
+
+
+x.remove(-514)
+print(x)
+
+
+# x.sort()
+# print(x)
+# x.sort(reverse=True)
+# print(x)
+
+x.reverse()
+print(x)
+
+
+
+# اسمع كل الي انت شفته مشلازم تفهمه عشان كذا دز وعمرك لا تتقبل ذا الملف ههههههه
