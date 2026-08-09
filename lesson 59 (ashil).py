53a8dc2e3fc5002e7f4e045c63cafde4629bcc1b
asyl68372-wq <asyl68372@gmail.com>
2026-08-09 08:02:24 +0000

    مشروع 59 فانكشن مع قيمه الافتراضيه

---
+++ lesson 59 (ashil).py
@@ -0,0 +1,14 @@
+#نحط فانكشن ونحط القيمه الافتراضيه فاضيه
+
+def info(name="unknown", age="unknown", country="unknown"):
+    print(f"Name: {name}, Age: {age}, Country: {country}")
+
+#نساله عن الحاجات عشان نملي القيمه الافتراضيه
+
+newname = input("what's your name :\n").capitalize().strip()
+agee = input("how older are you: \n").strip()
+countryy = input("where's are you from: \n").capitalize().strip()
+
+#نحط الاجوبه من الاسئله بدل القيمه الافتراضيه
+
+info(newname, agee, countryy)
