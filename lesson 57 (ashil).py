2cbd438993d24c055cc67efb548389ed7377fa5b
asyl68372-wq <asyl68372@gmail.com>
2026-08-09 07:05:29 +0000

    تدريب الفانكشن حلقة 57

---
+++ lesson 57 (ashil).py
@@ -0,0 +1,13 @@
+#بنجرب الفانكشن ع السريع 
+
+#نسوي الفانكشن
+def names(f, s, l):
+    print(f"hi {f.strip().capitalize()} {s.strip().upper():.1s} {l.strip().capitalize()}")
+
+#نساله لكل رساله لوحدها لاني فوق فصلت كل اسم من حرف f و s و l 
+bruh = input("hi what's your first name?\n")
+bru = input("hi what's your 2 name?\n")
+brh = input("hi what's your 3 name?\n")
+
+#نطبع الي داخل المتغيرات تبع الاسئله في اماكن ال f و s و l 
+names(bruh, bru, brh)
