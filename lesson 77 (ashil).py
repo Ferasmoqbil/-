f1f49c9415d0c503af6bdf0476eb1a9610aba768
asyl68372-wq <asyl68372@gmail.com>
2026-08-18 19:49:21 +0000

    الحلقة 77 ربط اكواد ب مشاريع اخرئ مع مشاريع اخرئ

---
+++ lesson 77 (ashil).py
@@ -0,0 +1,6 @@
+import lesson77testashil as test
+
+test.sayhello("ashil")
+test.sayhru("ashil")
+test.sayhello("feras")
+test.sayhru("feras")
---
+++ lesson77testashil.py
@@ -0,0 +1,5 @@
+def sayhello(name):
+    print(f"hello {name} ")
+    
+def sayhru(name):
+    print(f"how are you {name}? ")
