e2fa1f32508dddb46573cc6448bb5e57125e93d9
asyl68372-wq <asyl68372@gmail.com>
2026-08-21 05:47:17 +0000

    حلقة 89 تدريب علئ تقييم طريقة كتابة الكود

---
+++ lesson 89 (ashil).py
@@ -0,0 +1,15 @@
+'''
+this's func to say hello
+'''
+def say_hello(name):
+    '''
+    this print hello to someone
+    '''
+    print(f"hello {name}")
+
+say_hello("ashil")
+
+
+# هذا كود اختبار صحه الكود ب استخدام 
+# pylint "lesson 89 (ashil).py"
+# اذا طبعتها في التريمينال سيعطيك تقييم الكود من 10 وسيخبرك بما ينقصك 
